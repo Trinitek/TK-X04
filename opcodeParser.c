60ba72e4e15dfa4d33ce253df0c0a6a5b9fa7595
Trinitek <trinitek@live.com>
2014-09-29 23:57:47 -0400

    Add instruction set draft
    Also added stuff to opcode parser, will have to fix because the draft
    changed the way instruction parameter encoding works n stuff.

--- opcodeParser.c
+++ opcodeParser.c
@@ -26,6 +26,36 @@ bool parseOpcode(uint8_t opcode) {
     else if (opcode <= 0b00011111 && opcode >= 0b00010000) {
         switch(opcode) {
             default: return false;
+            case 0b00010000:    // MOV reg, reg
+                break;
+            case 0b00010001:    // MOV [reg], reg
+                break;
+            case 0b00010010:    // MOV reg, [reg]
+                break;
+            case 0b00010011:    // MOV reg, imm
+                break;
+            case 0b00010100:    // MOV [imm], reg
+                break;
+            case 0b00010101:    // MOV reg, [imm]
+                break;
+            case 0b00010110:    // XCHG reg, reg
+                break;
+            case 0b00010111:    // LOADX
+                break;
+            case 0b00011000:    // LOADY
+                break;
+            case 0b00011001:    // LOADZ
+                break;
+            case 0b00011010:    // STOX
+                break;
+            case 0b00011011:    // STOY
+                break;
+            case 0b00011100:    // STOZ
+                break;
+            case 0b00011101:    // MOVXY
+                break;
+            case 0b00011110:    // MOVYX
+                break;
         } return true;
     }
 
@@ -33,6 +63,28 @@ bool parseOpcode(uint8_t opcode) {
     else if (opcode <= 0b00101111 && opcode >= 0b00100000) {
         switch(opcode) {
             default: return false;
+            case 0b00100000:    // SHR reg, imm
+                break;
+            case 0b00100001:    // SHL reg, imm
+                break;
+            case 0b00100010:    // ROR reg, imm
+                break;
+            case 0b00100011:    // ROL reg, imm
+                break;
+            case 0b00100100:    // AND reg, reg
+                break;
+            case 0b00100101:    // OR reg, reg
+                break;
+            case 0b00100110:    // NOT reg, reg
+                break;
+            case 0b00100111:    // NAND reg, reg
+                break;
+            case 0b00101000:    // NOR reg, reg
+                break;
+            case 0b00101001:    // XOR reg, reg
+                break;
+            case 0b00101010:    // XNOR reg, reg
+                break;
         } return true;
     }
 
