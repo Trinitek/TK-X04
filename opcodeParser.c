29b5717af303a8aeb3955a0a43ba02b8f97d0f80
Trinitek <trinitek@live.com>
2014-10-01 21:43:36 -0400

    Correct, finish parseOpcode structure
    Adjusted existing opcode checking for the updated instruction set draft,
    and added the remaining ones.

--- opcodeParser.c
+++ opcodeParser.c
@@ -15,9 +15,9 @@ bool parseOpcode(uint8_t opcode) {
                 break;
             case 0b00001011:    // STC
                 break;
-            case 0b00001100:    // CMP reg, reg
+            case 0b00001100:    // CMP
                 break;
-            case 0b00001101:    // CMP reg, imm
+            case 0b00001101:    // REP
                 break;
         } return true;
     }
@@ -26,35 +26,15 @@ bool parseOpcode(uint8_t opcode) {
     else if (opcode <= 0b00011111 && opcode >= 0b00010000) {
         switch(opcode) {
             default: return false;
-            case 0b00010000:    // MOV reg, reg
+            case 0b00010000:    // MOV
                 break;
-            case 0b00010001:    // MOV [reg], reg
+            case 0b00010001:    // XCHG
                 break;
-            case 0b00010010:    // MOV reg, [reg]
+            case 0b00010010:    // LOADP
                 break;
-            case 0b00010011:    // MOV reg, imm
+            case 0b00010011:    // STOREP
                 break;
-            case 0b00010100:    // MOV [imm], reg
-                break;
-            case 0b00010101:    // MOV reg, [imm]
-                break;
-            case 0b00010110:    // XCHG reg, reg
-                break;
-            case 0b00010111:    // LOADX
-                break;
-            case 0b00011000:    // LOADY
-                break;
-            case 0b00011001:    // LOADZ
-                break;
-            case 0b00011010:    // STOX
-                break;
-            case 0b00011011:    // STOY
-                break;
-            case 0b00011100:    // STOZ
-                break;
-            case 0b00011101:    // MOVXY
-                break;
-            case 0b00011110:    // MOVYX
+            case 0b00010100:    // MOVEP
                 break;
         } return true;
     }
@@ -63,27 +43,27 @@ bool parseOpcode(uint8_t opcode) {
     else if (opcode <= 0b00101111 && opcode >= 0b00100000) {
         switch(opcode) {
             default: return false;
-            case 0b00100000:    // SHR reg, imm
+            case 0b00100000:    // SHR
                 break;
-            case 0b00100001:    // SHL reg, imm
+            case 0b00100001:    // SHL
                 break;
-            case 0b00100010:    // ROR reg, imm
+            case 0b00100010:    // ROR
                 break;
-            case 0b00100011:    // ROL reg, imm
+            case 0b00100011:    // ROL
                 break;
-            case 0b00100100:    // AND reg, reg
+            case 0b00100100:    // AND
                 break;
-            case 0b00100101:    // OR reg, reg
+            case 0b00100101:    // OR
                 break;
-            case 0b00100110:    // NOT reg, reg
+            case 0b00100110:    // NOT
                 break;
-            case 0b00100111:    // NAND reg, reg
+            case 0b00100111:    // NAND
                 break;
-            case 0b00101000:    // NOR reg, reg
+            case 0b00101000:    // NOR
                 break;
-            case 0b00101001:    // XOR reg, reg
+            case 0b00101001:    // XOR
                 break;
-            case 0b00101010:    // XNOR reg, reg
+            case 0b00101010:    // XNOR
                 break;
         } return true;
     }
@@ -92,6 +72,30 @@ bool parseOpcode(uint8_t opcode) {
     else if (opcode <= 0b00111111 && opcode >= 0b00110000) {
         switch(opcode) {
             default: return false;
+            case 0b00110000:    // JMP
+                break;
+            case 0b00110001:    // JC
+                break;
+            case 0b00110010:    // JNC
+                break;
+            case 0b00110011:    // JG
+                break;
+            case 0b00110100:    // JLE
+                break;
+            case 0b00110101:    // JL
+                break;
+            case 0b00110110:    // JGE
+                break;
+            case 0b00110111:    // JE
+                break;
+            case 0b00111000:    // JNE
+                break;
+            case 0b00111001:    // LOOP
+                break;
+            case 0b00111010:    // LOOPE
+                break;
+            case 0b00111011:    // LOOPNE
+                break;
         } return true;
     }
 
@@ -99,6 +103,22 @@ bool parseOpcode(uint8_t opcode) {
     else if (opcode <= 0b01011111 && opcode >= 0b01000000) {
         switch(opcode) {
             default: return false;
+            case 0b01000000:    // INC
+                break;
+            case 0b01000001:    // DEC
+                break;
+            case 0b01000010:    // ADD
+                break;
+            case 0b01000011:    // ADC
+                break;
+            case 0b01000100:    // SUB
+                break;
+            case 0b01000101:    // SBB
+                break;
+            case 0b01000110:    // MUL
+                break;
+            case 0b01000111:    // DIV
+                break;
         } return true;
     }
 
@@ -106,6 +126,18 @@ bool parseOpcode(uint8_t opcode) {
     else if (opcode <= 0b01100111 && opcode >= 0b01100000) {
         switch(opcode) {
             default: return false;
+            case 0b01100000:    // PUSHA
+                break;
+            case 0b01100001:    // PUSH
+                break;
+            case 0b01100010:    // POPA
+                break;
+            case 0b01100011:    // POP
+                break;
+            case 0b01100100:    // CALL
+                break;
+            case 0b01100101:    // RET
+                break;
         } return true;
     }
 
@@ -113,6 +145,10 @@ bool parseOpcode(uint8_t opcode) {
     else if (opcode <= 0b01101111 && opcode >= 0b01101000) {
         switch(opcode) {
             default: return false;
+            case 0b01101000:    // IN
+                break;
+            case 0b01101001:    // OUT
+                break;
         } return true;
     }
 
@@ -120,6 +156,14 @@ bool parseOpcode(uint8_t opcode) {
     else if (opcode <= 0b01110111 && opcode >= 0b01110000) {
         switch(opcode) {
             default: return false;
+            case 0b01110000:    // STI
+                break;
+            case 0b01110001:    // CLI
+                break;
+            case 0b01110010:    // IRET
+                break;
+            case 0b01110011:    // INT
+                break;
         } return true;
     }
     
