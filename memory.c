7a0131636d61fa25c6e8e8e42e6767865ac16c39
Trinitek <trinitek@live.com>
2014-10-05 20:09:14 -0400

    Add memory access functions

---
+++ memory.c
@@ -0,0 +1,18 @@
+
+#include <stdint.h>
+
+uint8_t getMemByte(uint16_t pointer) {
+    return 0;
+}
+
+uint16_t getMemWord(uint16_t pointer) {
+    return 0;
+}
+
+void setMemByte(uint16_t pointer, uint8_t data) {
+    //
+}
+
+void setMemWord(uint16_t pointer, uint16_t data) {
+    //
+}
---
+++ memory.h
@@ -0,0 +1,5 @@
+
+uint8_t getMemByte(uint16_t pointer);
+uint16_t getMemWord(uint16_t pointer);
+void setMemByte(uint16_t pointer, uint8_t data);
+void setMemWord(uint16_t pointer, uint16_t data);
